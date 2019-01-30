b42420418c35629e1129cafb227eb09731f1c2a8
Dan Barkhorn <danielbarkhorn@gmail.com>
2019-01-30 13:14:26 -0800

    chore: removing uneeded link

--- override/artbuffer.js
+++ override/artbuffer.js
@@ -3,10 +3,12 @@ function artbuffer(){
 	var url_base = "https://www.artic.edu/artworks/";
 	var base = 1000;
 	var max = 1000;
-	var rand_id = base + Math.random() * Math.floor(max));
+	var rand_id = base + Math.random() * Math.floor(max);
 	url_base += rand_id;
 	
 	document.innerHTML = "";
 	document.write(url_base);
 	
 }
+
+artbuffer();
