469993f0db3cee412f2b7a0dabf6d0a2f90e8bb0
Dan Barkhorn <danielbarkhorn@gmail.com>
2019-08-20 17:22:41 -0700

    task: working product with hardcoded links

---
+++ override/artworkSelector.js
@@ -0,0 +1,59 @@
+art = [
+{"title": "A Sunday on La Grande Jatte \u2014 1884, 1884/86", "link": "https://www.artic.edu/artworks/27992/a-sunday-on-la-grande-jatte-1884?page=1", "imgLink": "https://www.artic.edu/iiif/2/88d65792-afb3-c90b-56d7-6f95967fa731", "author": "Georges Seurat"},
+{"title": "Nighthawks, 1942", "link": "https://www.artic.edu/artworks/111628/nighthawks?page=1", "imgLink": "https://www.artic.edu/iiif/2/39d43108-e690-2705-67e2-a16dc28b8c7f", "author": "Edward Hopper"},
+{"title": "American Gothic, 1930", "link": "https://www.artic.edu/artworks/6565/american-gothic?page=1", "imgLink": "https://www.artic.edu/iiif/2/d02e0079-8e82-733e-683c-cb83a387ee5e", "author": "Grant Wood"},
+{"title": "The Bedroom, 1889", "link": "https://www.artic.edu/artworks/28560/the-bedroom?page=1", "imgLink": "https://www.artic.edu/iiif/2/6ad8f7d6-c8a9-5216-2664-09bd5691c6ed", "author": "Vincent van Gogh"},
+{"title": "Nightlife, 1943", "link": "https://www.artic.edu/artworks/117266/nightlife?page=1", "imgLink": "https://www.artic.edu/iiif/2/3a8b3bbd-9bd2-43b5-aec2-f22b6d55cdf1", "author": "Archibald John Motley Jr."},
+{"title": "The Old Guitarist, late 1903\u2013early 1904", "link": "https://www.artic.edu/artworks/28067/the-old-guitarist?page=1", "imgLink": "https://www.artic.edu/iiif/2/869a7319-306e-ca90-1732-cb614199f3df", "author": "Pablo Picasso"},
+{"title": "Paris Street; Rainy Day, 1877", "link": "https://www.artic.edu/artworks/20684/paris-street-rainy-day?page=1", "imgLink": "https://www.artic.edu/iiif/2/7c1093fb-f82c-02aa-a743-b179f26fb89a", "author": "Gustave Caillebotte"},
+{"title": "Buddha Shakyamuni Seated in Meditation (Dhyanamudra), Chola period, about 12th century", "link": "https://www.artic.edu/artworks/21023/buddha-shakyamuni-seated-in-meditation-dhyanamudra?page=1", "imgLink": "https://www.artic.edu/iiif/2/a1a694d2-15e5-2464-0f5b-f527f7bfbfd4", "author": null},
+{"title": "The Assumption of the Virgin, 1577\u201379", "link": "https://www.artic.edu/artworks/87479/the-assumption-of-the-virgin?page=1", "imgLink": "https://www.artic.edu/iiif/2/9feeeedc-359f-52ea-7c08-c8589549fc7b", "author": "Domenico Theotok\u00f3poulos, called El Greco"},
+{"title": "America Windows, 1977", "link": "https://www.artic.edu/artworks/109439/america-windows?page=1", "imgLink": "https://www.artic.edu/iiif/2/a62a08bb-6775-dc38-330e-8fae4271d9a4", "author": "Marc Chagall"},
+{"title": "Coronation Stone of Motecuhzoma II (Stone of the Five Suns), 1503", "link": "https://www.artic.edu/artworks/75644/coronation-stone-of-motecuhzoma-ii-stone-of-the-five-suns?page=1", "imgLink": "https://www.artic.edu/iiif/2/9b059a28-af7d-2ecf-2dfb-a2a9a06d5374", "author": "Aztec (Mexica)"},
+{"title": "City Landscape, 1955", "link": "https://www.artic.edu/artworks/86385/city-landscape?page=1", "imgLink": "https://www.artic.edu/iiif/2/3f7b2a2e-ea6c-0834-9c13-87b96e98a68a", "author": "Joan Mitchell"},
+{"title": "Bathers by a River, 1909\u201310, 1913, and 1916\u20131917", "link": "https://www.artic.edu/artworks/79307/bathers-by-a-river?page=1", "imgLink": "https://www.artic.edu/iiif/2/2f2fb68f-7a92-5805-deed-a57c5aaec251", "author": "Henri Matisse"},
+{"title": "Stacks of Wheat (End of Summer), 1890/91", "link": "https://www.artic.edu/artworks/64818/stacks-of-wheat-end-of-summer?page=1", "imgLink": "https://www.artic.edu/iiif/2/9511bf7d-4f6c-f523-5fb1-ff069c17f16c", "author": "Claude Monet"},
+{"title": "Veranda Post (Opo Ogoga), 1910/14", "link": "https://www.artic.edu/artworks/102611/veranda-post-opo-ogoga?page=1", "imgLink": "https://www.artic.edu/iiif/2/cb094c5a-79b4-4eaf-f05b-4ab73f9e2fb9", "author": "Olowe of Ise"},
+{"title": "Starry Night and the Astronauts, 1972", "link": "https://www.artic.edu/artworks/129884/starry-night-and-the-astronauts?page=1", "imgLink": "https://www.artic.edu/iiif/2/662912d3-e51c-0572-f298-2e8a43b4b21a", "author": "Alma Thomas"},
+{"title": "Target, 1961", "link": "https://www.artic.edu/artworks/229351/target?page=1", "imgLink": "https://www.artic.edu/iiif/2/c4276275-66b8-d931-860e-7cfd16512218", "author": "Jasper Johns"},
+{"title": "Untitled, 1964", "link": "https://www.artic.edu/artworks/229393/untitled?page=1", "imgLink": "https://www.artic.edu/iiif/2/12a1920e-3b7a-a87d-1b9d-5574fbb02118", "author": "Tanaka Atsuko"},
+{"title": "Water Lilies, 1906", "link": "https://www.artic.edu/artworks/16568/water-lilies?page=1", "imgLink": "https://www.artic.edu/iiif/2/adc61da8-d020-d84f-62fb-f5c7435f1f98", "author": "Claude Monet"},
+{"title": "The Weaver, 1936", "link": "https://www.artic.edu/artworks/151363/the-weaver?page=1", "imgLink": "https://www.artic.edu/iiif/2/600c0f27-1ab8-152e-7f9f-f6264150eb51", "author": "Diego Rivera"},
+{"title": "Still Life with Geranium, 1906", "link": "https://www.artic.edu/artworks/87045/still-life-with-geranium?page=12", "imgLink": "https://www.artic.edu/iiif/2/b11b21e0-8a59-06c6-c300-ae4d2e113ea0", "author": "Henri Matisse"},
+{"title": "Mural Fragment Representing a Ritual of World Renewal, A.D. 500/600", "link": "https://www.artic.edu/artworks/14968/mural-fragment-representing-a-ritual-of-world-renewal?page=12", "imgLink": "https://www.artic.edu/iiif/2/27a01e3c-9461-159e-cc28-f837086cb221", "author": "Teotihuacan"},
+{"title": "Movements, 1913", "link": "https://www.artic.edu/artworks/65916/movements?page=12", "imgLink": "https://www.artic.edu/iiif/2/2bf67f4e-0896-7aa5-39e2-22f2bd8c2736", "author": "Marsden Hartley"},
+{"title": "Mrs. Daniel Hubbard (Mary Greene), c. 1764", "link": "https://www.artic.edu/artworks/59787/mrs-daniel-hubbard-mary-greene?page=12", "imgLink": "https://www.artic.edu/iiif/2/4e0e0506-5964-2741-8d09-36b32c881d53", "author": "John Singleton Copley"},
+{"title": "Cauldron, Western Zhou dynasty (1046\u2013771 BC ), early 9th century BC", "link": "https://www.artic.edu/artworks/185963/cauldron?page=12", "imgLink": "https://www.artic.edu/iiif/2/411dcae3-cb6e-1eb7-263c-139062841a9e", "author": null},
+{"title": "The Annunciation, c. 1583", "link": "https://www.artic.edu/artworks/44741/the-annunciation?page=12", "imgLink": "https://www.artic.edu/iiif/2/16a5743a-c933-cd7f-87aa-88b3456db07b", "author": "Alessandro Vittoria"},
+{"title": "TAKEN FROM HERE TO WHERE IT CAME FROM AND TAKEN TO A PLACE AND USED IN SUCH A MANNER THAT IT CAN ONLY REMAIN AS A REPRESENTATION OF WHAT IT WAS WHERE IT CAME FROM, 1980", "link": "https://www.artic.edu/artworks/107069/taken-from-here-to-where-it-came-from-and-taken-to-a-place-and-used-in-such-a-manner-that-it-can-only-remain-as-a-representation-of-what-it-was-where-it-came-from?page=12", "imgLink": "https://www.artic.edu/iiif/2/cfb4ebd9-8704-5e63-f236-f1c51ff08c95", "author": "Lawrence Weiner"},
+{"title": "Ceres, 1928", "link": "https://www.artic.edu/artworks/63178/ceres?page=12", "imgLink": "https://www.artic.edu/iiif/2/4886e9b6-0a70-743a-c363-2e39cff0562e", "author": "John Bradley Storrs"},
+{"title": "Night Sky #2, 1991", "link": "https://www.artic.edu/artworks/140645/night-sky-2?page=12", "imgLink": "https://www.artic.edu/iiif/2/fe989fa2-baf7-d127-170e-942b5f3987f9", "author": "Vija Celmins"},
+{"title": "View of Delphi with a Procession, 1673", "link": "https://www.artic.edu/artworks/43145/view-of-delphi-with-a-procession?page=12", "imgLink": "https://www.artic.edu/iiif/2/47a827a3-3697-1d81-5ae4-f8f0b48ecbcb", "author": "Claude Lorrain"},
+{"title": "Two Ladies at the Automat (New York City), 1966, printed 1977", "link": "https://www.artic.edu/artworks/63234/two-ladies-at-the-automat-new-york-city?page=12", "imgLink": "https://www.artic.edu/iiif/2/f433c0e4-e4d7-e6a5-f946-68e17d546e8c", "author": "Diane Arbus"},
+{"title": "The Ballad of Sexual Dependency, 1979/2001", "link": "https://www.artic.edu/artworks/187155/the-ballad-of-sexual-dependency?page=12", "imgLink": "https://www.artic.edu/iiif/2/d825e6af-6019-111b-b17d-039bf08a8338", "author": "Nan Goldin"},
+{"title": "Triptych with Scenes from the Life of Christ, 1350/75", "link": "https://www.artic.edu/artworks/25332/triptych-with-scenes-from-the-life-of-christ?page=12", "imgLink": "https://www.artic.edu/iiif/2/3ec4e387-ffd0-6770-6905-9cc6e1c0b988", "author": null},
+{"title": "Chalk-Mirror Displacement, 1969", "link": "https://www.artic.edu/artworks/93345/chalk-mirror-displacement?page=12", "imgLink": "https://www.artic.edu/iiif/2/003dde75-ec7c-82c0-38ac-ae96002f4c54", "author": "Robert Smithson"},
+{"title": "Monk (M\u00f6nch), 1997/99", "link": "https://www.artic.edu/artworks/154232/monk-monch?page=12", "imgLink": "https://www.artic.edu/iiif/2/3d9aa8e4-8944-9711-3081-43946d4e369d", "author": "Katharina Fritsch"},
+{"title": "Tanktotem I, 1952", "link": "https://www.artic.edu/artworks/79379/tanktotem-i?page=12", "imgLink": "https://www.artic.edu/iiif/2/236995cd-e505-4c08-5fbf-1d4b4f502487", "author": "David Smith"},
+{"title": "Evening Snow on a Floss Shaper (Nurioke no bosetsu), from the series \u201cEight Views of the Parlor (Zashiki hakkei)\u201d, c. 1766", "link": "https://www.artic.edu/artworks/88977/evening-snow-on-a-floss-shaper-nurioke-no-bosetsu-from-the-series-eight-views-of-the-parlor-zashiki-hakkei?page=12", "imgLink": "https://www.artic.edu/iiif/2/d67a475d-658d-253d-fba1-86abc6aae201", "author": "Suzuki Harunobu"},
+{"title": "The Young Emperor Akbar Arrests the Insolent Shah Abu\u2019l-Maali, page from a manuscript of the Akbarnama, Mughal period, c. 1590/95", "link": "https://www.artic.edu/artworks/76816/the-young-emperor-akbar-arrests-the-insolent-shah-abu-l-maali-page-from-a-manuscript-of-the-akbarnama?page=12", "imgLink": "https://www.artic.edu/iiif/2/2c75576a-dbae-8e62-dc37-02cc379d63d0", "author": "Basawan"},
+{"title": "Untitled, 1958", "link": "https://www.artic.edu/artworks/146991/untitled?page=12", "imgLink": "https://www.artic.edu/iiif/2/9c90d8e8-d38f-098f-82b2-ec1b3c48f9d5", "author": "Clyfford Still"},
+{"title": "M\u00e8re Gr\u00e9goire, 1855 and 1857/59", "link": "https://www.artic.edu/artworks/4428/mere-gregoire?page=12", "imgLink": "https://www.artic.edu/iiif/2/ebf4061d-9b87-ec9a-d767-003a0b9f1cbb", "author": "Gustave Courbet"},
+{"title": "Spanish Dancer, 1920s", "link": "https://www.artic.edu/artworks/37368/spanish-dancer?page=13", "imgLink": "https://www.artic.edu/iiif/2/2c125a0a-7e4a-3b2a-5748-8d352485d93a", "author": "Natalia Goncharova"},
+]
+
+function artworkSelector(){
+	var numArtPieces = 41
+	var index = parseInt(Math.random() * numArtPieces);
+	var height = (window.innerHeight * 2).toString();
+	console.log(height);
+	var linkEnding = "/full/".concat(height, ",/0/default.jpg");
+	var imgLink = art[index].imgLink.concat(linkEnding);
+
+
+	document.getElementsByClassName('artwork-container__artwork')[0].src = imgLink;
+	document.getElementsByClassName('artwork-container__title')[0].innerHTML = art[index].title;
+	document.getElementsByClassName('artwork-container__subtitle')[0].innerHTML = art[index].author;
+}
+
+artworkSelector();
